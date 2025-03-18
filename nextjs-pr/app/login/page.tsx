6506ddc1419a21bd16207c0202288fe727b6ba60
wjenaro <wjenaro@gmail.com>
2025-03-18 23:59:18 +0300

    practicing groupded routes

---
+++ nextjs-pr/app/login/page.tsx
@@ -0,0 +1,7 @@
+
+
+export default function Login() {
+    return <h1 className="animate-ping text-9xl"> This is login</h1>;
+    
+  }
+  
---
+++ nextjs-pr/app/register/page.tsx
@@ -0,0 +1,4 @@
+export default function Register() {
+  return <h1 className="text-9xl animate-bounce"> This is the Register Page</h1>;
+  
+}
